325ca129d1f5e00f0a6645ea7b1cda7700039410
Durjoy Barua <durjoybarua8115@example.com>
2024-08-07 11:44:38 +0600

    push

--- 6.py
+++ 6.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
 response = requests.get("https://www.tutorialsfreak.com/")
@@ -24,31 +23,4 @@ for i in img:
 print("\n\n\n\n")
 ancher= soup.find_all("a")
 for a in ancher:
-=======
-import requests
-from bs4 import BeautifulSoup
-response = requests.get("https://www.tutorialsfreak.com/")
-print(response)
-print(response.status_code)
-
-soup = BeautifulSoup(response.content , "html.parser")
-
-
-# in this episode we will show you how to pull link 
-
-img = soup.find_all("img")
-# hence there can many images that's why will will terminate a loop
-
-
-for i in img:
-    #we know that in img tag there is a src where the link provide
-    print(i.get("src"))
-
-
-# as well as we can pull link from ancher(a) 
-# in ancher tag link privide in href 
-print("\n\n\n\n")
-ancher= soup.find_all("a")
-for a in ancher:
->>>>>>> c834a84429e2884b7d393b60c550d04c027ba19e
     print(a.get("href"))
