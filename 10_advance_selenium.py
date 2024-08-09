dce80cf64331587ff3b37bae20429541746f681c
Durjoy Barua <durjoybarua8115@example.com>
2024-08-09 21:46:10 +0600

    push

---
+++ 10_advance_selenium.py
@@ -0,0 +1,36 @@
+ # Import webdriver from Selenium library
+from selenium import webdriver
+
+ # Import Service class to manage ChromeDriver
+from selenium.webdriver.chrome.service import Service 
+import time
+
+# Path to ChromeDriver executable
+path = "K:/chromedriver-win64/chromedriver.exe"
+
+# Create a Service object for ChromeDriver
+s = Service(path)
+
+# Initialize Chrome WebDriver using the Service object
+driver = webdriver.Chrome(service=s)  
+
+# Navite to Google homepage
+driver.get("https://www.google.com/")  
+
+# Locate Google search box using XPath
+
+box = driver.find_element_by_xpath('//*[@id="APjFqb"]')
+
+# Type "house of dragon" into the search box
+box.send_keys("house of dragon")
+
+# Submit the search form 
+box.submit()
+
+time.sleep(2)  
+
+driver.find_element_by_xpath('//*[@id="kp-wp-tab-overview"]/div[6]/div/div/div/div/div/div[1]/div/div/span/a').click() 
+
+time.sleep(2)
+
+driver.save_screenshot("F:/Web_Scraping/1st.png")
